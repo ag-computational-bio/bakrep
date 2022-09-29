0b29f1510663341e93426716ff18de49bc7ed038
Linda Fenske <lfenske@hp-dl560.internal.computational.bio.uni-giessen.de>
2022-09-29 12:24:25 +0200

    Add script to parse the checkm2 results.

---
+++ nextflow/bin/ParseToJSON_checkm2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import json
+from logging import root
+import sys
+import os
+import pandas as pd
+import numpy as np
+from argparse import ArgumentParser
+
+def np_encoder(object):
+    if isinstance(object, np.generic):
+        return object.item()
+
+parser = ArgumentParser(description="Creates JSON file from checkm2 results.")
+
+parser.add_argument("-i", dest="input", default="checkm.tsv", type=str, help="Name of the input file (default: checkm.tsv)")
+#parser.add_argument("-o", dest="output", default="/vol/bakrep/linda_test/no_backup/nextflow/", type=str, help="Name of the output dir.")
+args=parser.parse_args()
+
+checkm2_data=pd.read_csv(args.input,sep='\t', names=["Name","Completeness","Contamination","Completeness_Model_Used","Translation_Table_Used","Additional_Notes"], skiprows=1)
+
+for line in range(0,checkm2_data.shape[0]):
+    checkm2_dict={"quality": {"completeness": checkm2_data.Completeness[line], "contamination": checkm2_data.Contamination[line]}, 
+                "calculation": {"model": checkm2_data.Completeness_Model_Used[line], "translation_table": checkm2_data.Translation_Table_Used[line], "notes": checkm2_data.Additional_Notes[line]}} 
+
+    checkm2_json=json.dumps(checkm2_dict, default=np_encoder)
+
+    #jsonFile = checkm_data.BinID[line][3:7] + "/" + checkm_data.BinID[line].split('.')[0] + ".checkm.json"
+    jsonFile = "checkm2.json"
+    #os.makedirs(os.path.dirname(jsonFile), exist_ok=True)
+    with open(jsonFile, "w") as file:
+        file.write(checkm2_json)
