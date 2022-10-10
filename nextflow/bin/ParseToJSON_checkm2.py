d2e052aba14719a008e962bf06e04c1e8e7042d4
Linda Fenske <lfenske@hp-dl560.internal.computational.bio.uni-giessen.de>
2022-10-10 15:17:31 +0200

    Added output argument (jsonFile = args.output).

--- nextflow/bin/ParseToJSON_checkm2.py
+++ nextflow/bin/ParseToJSON_checkm2.py
@@ -14,8 +14,8 @@ def np_encoder(object):
 
 parser = ArgumentParser(description="Creates JSON file from checkm2 results.")
 
-parser.add_argument("-i", dest="input", default="checkm.tsv", type=str, help="Name of the input file (default: checkm.tsv)")
-#parser.add_argument("-o", dest="output", default="/vol/bakrep/linda_test/no_backup/nextflow/", type=str, help="Name of the output dir.")
+parser.add_argument("-i", dest="input", default="checkm2.tsv", type=str, help="Name of the input file (default: checkm.tsv)")
+parser.add_argument("-o", dest="output", default="checkm2.json", type=str, help="Name of the output file (default: checkm2.json).")
 args=parser.parse_args()
 
 checkm2_data=pd.read_csv(args.input,sep='\t', names=["Name","Completeness","Contamination","Completeness_Model_Used","Translation_Table_Used","Additional_Notes"], skiprows=1)
@@ -27,7 +27,7 @@ for line in range(0,checkm2_data.shape[0]):
     checkm2_json=json.dumps(checkm2_dict, default=np_encoder)
 
     #jsonFile = checkm_data.BinID[line][3:7] + "/" + checkm_data.BinID[line].split('.')[0] + ".checkm.json"
-    jsonFile = "checkm2.json"
+    jsonFile = args.output
     #os.makedirs(os.path.dirname(jsonFile), exist_ok=True)
     with open(jsonFile, "w") as file:
         file.write(checkm2_json)
